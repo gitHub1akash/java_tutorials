158f5c22fb75146ecc7c2073cd19590546a195e3
aadionweb07 <108791017+aadionweb07@users.noreply.github.com>
2022-10-18 14:43:10 +0530

    Update binarysearch2.cpp

--- binarysearch2.cpp
+++ binarysearch2.cpp
@@ -2,27 +2,27 @@
 #include<conio.h>
 #include<stdbool.h>
 int main(){
-	int n,search,mid;
+	int n,key,mid;
 	bool found = false;
-	printf("enter the number of elements : ");
+	printf("enter the number of elements in the array : ");
 	scanf("%d",&n);
 	
 	int a[n];
-	printf("\nenter the sorted array:\n");
+	printf("\n : enter the sorted array: \n");
 	for(int i=0;i<n;i++){
 		scanf("%d",&a[i]);
 	}
 
-	printf("\nto search : ");
-	scanf("%d",&search);
+	printf("\nto key: ");
+	scanf("%d",&key);
 	int lI=0,uI=n-1;
 	while(lI<=uI){
 		mid = (lI+uI)/2;
-		if(search == a[mid]){
+		if(key == a[mid]){
 			found = true;
 			break;
 		}
-		else if(search < a[mid]){
+		else if(key < a[mid]){
 			uI = mid-1;
 		}
 		else{
@@ -30,9 +30,9 @@ int main(){
 		}
 	}
 	if(found) 
-	printf("\n%d is found at index no. %d",search,mid);
+	printf("\n%d is found at index no. %d",key,mid);
 	else
-	printf("\n%d is not found",search);
+	printf("\n%d is not found",key);
 	
 
 
