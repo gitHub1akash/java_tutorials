b34c03769bb7c72f5a7527d55f0fab99c5466893
Sneha Basak <88227428+Sneha0008@users.noreply.github.com>
2022-10-19 10:45:44 +0530

    Update binarysearch2.cpp

--- binarysearch2.cpp
+++ binarysearch2.cpp
@@ -4,10 +4,12 @@
 int main(){
 	int n,key,mid;
 	bool found = false;
+	//added elements of arrays
 	printf("enter the number of elements in the array : ");
 	scanf("%d",&n);
 	
 	int a[n];
+	//added sortedarray elements
 	printf("\n : enter the sorted array: \n");
 	for(int i=0;i<n;i++){
 		scanf("%d",&a[i]);
